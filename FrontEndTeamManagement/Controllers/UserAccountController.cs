7218df75da387c60f53ed49a0e2dcaa896710f86
Komal Navale <komalniteshnavale@outlook.com>
2021-07-07 15:25:43 +0530

    Initial Commit

---
+++ FrontEndTeamManagement/Controllers/UserAccountController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using FrontEndTeamManagement.Models;
+
+namespace FrontEndTeamManagement.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        //
+        // GET: /UserAccount/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["FrontEndTeamManagementConnection"].ToString();
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da1 = new SqlDataAdapter("Select * From tblGender", con);
+            SqlDataAdapter da2 = new SqlDataAdapter("Select * From tblSecQuestion", con);
+            SqlDataAdapter da3 = new SqlDataAdapter("Select * From tblTeamRole", con);
+            SqlDataAdapter da4 = new SqlDataAdapter("Select * From tblMaritalStatus", con);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            ViewBag.Gender = ToSelectList(dt1, "ID", "Gender");
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            ViewBag.QuestionList = ToSelectList(dt2, "ID", "Security_Questions");
+            DataTable dt3 = new DataTable();
+            da3.Fill(dt3);
+            ViewBag.TeamRoleList = ToSelectList(dt3, "ID", "TeamRole");
+            DataTable dt4 = new DataTable();
+            da4.Fill(dt4);
+            ViewBag.MaritalStatusList = ToSelectList(dt4, "ID", "MaritalStatus");
+
+            return View();
+        }
+
+        [NonAction]
+        public SelectList ToSelectList(DataTable table, string valueField, string textField)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                list.Add(new SelectListItem()
+                {                   
+                    Value = row[valueField].ToString(),
+                    Text = row[textField].ToString()
+                });
+            }
+
+            return new SelectList(list, "Value", "Text");
+        }
+
+        [HttpPost]
+        public ActionResult SaveRegistrationDetails(RegisterViewModel registerModel)
+        {
+            //Database Connection
+            string constr = ConfigurationManager.ConnectionStrings["FrontEndTeamManagementConnection"].ToString();
+            SqlConnection con = new SqlConnection(constr);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("AddTeamMemberDetailsProc", con);
+                    con.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmployeeId", registerModel.EmployeeId);
+                    cmd.Parameters.AddWithValue("@FirstName", registerModel.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", registerModel.LastName);
+                    cmd.Parameters.AddWithValue("@Gender", registerModel.Gender);
+                    cmd.Parameters.AddWithValue("@MaritalStatus", registerModel.MaritalStatus);
+                    cmd.Parameters.AddWithValue("@DateOfBirth", registerModel.DateOfBirth);
+                    cmd.Parameters.AddWithValue("@CurrentAddress", registerModel.CurrentAddress);
+                    cmd.Parameters.AddWithValue("@PinCode", registerModel.PinCode);
+                    cmd.Parameters.AddWithValue("@PermanentAddress", registerModel.PermanentAddress);
+                    cmd.Parameters.AddWithValue("@MobileNo", registerModel.MobileNo);
+                    cmd.Parameters.AddWithValue("@TeamRole", registerModel.TeamRole);
+                    cmd.Parameters.AddWithValue("@CompanyName", registerModel.CompanyName);
+                    cmd.Parameters.AddWithValue("@Experience", registerModel.WorkExperience);
+                    cmd.Parameters.AddWithValue("@TechnologyKnown", registerModel.TechnologyKnown);
+                    cmd.Parameters.AddWithValue("@Email", registerModel.EmailId);
+                    cmd.Parameters.AddWithValue("@UserName", registerModel.UserName);
+                    cmd.Parameters.AddWithValue("@usrPassword", registerModel.Password);
+                    cmd.Parameters.AddWithValue("@SecurityQuestion", registerModel.SecurityQuestion);
+                    cmd.Parameters.AddWithValue("@SecurityAnswer", registerModel.SecurityAnswer);
+                    //cmd.Parameters.AddWithValue("@tid", 0);
+                    //cmd.Parameters["@eid"].Direction = ParameterDirection.Output;
+
+                    //int i = cmd.ExecuteNonQuery();
+                    //i = Convert.ToInt16(cmd.Parameters["@eid"].Value);
+                    //con.Close();
+                    //if (i >= 1)
+                    //{
+
+                    //    ViewBag.Message = "Registered Successfully";
+
+                    //}
+                    //return View("Register");
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    ViewBag.Message = "Registered Successfully";
+                    return RedirectToAction("Login","UserAccount");
+                }
+                catch (Exception ex)
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                    ViewBag.result = ex.Message;
+                    return View("Register");
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                return View("Register", registerModel);
+            }
+
+        }
+	}
+}
---
+++ FrontEndTeamManagement/Models/UserAccountManagementModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontEndTeamManagement.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [RegularExpression(@"^[^<>.,?;:'()!~%\-_@#/*""\s]+$", ErrorMessage = "Special characters are not allowed.")]
+        [Display(Name = "Username")]
+        [StringLength(15, ErrorMessage = "More than 15 characters are not allowed")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password")]
+        [StringLength(15, ErrorMessage = "The {0} must be {2} to 15 characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Employee ID is required")]
+        [RegularExpression(@"^[^<>.,?;:'()!~%\-_@#/*""\s]+$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(20, ErrorMessage = "More than 20 characters are not allowed.")]
+        [Display(Name = "Employee ID")]
+        public string EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [RegularExpression(@"^[^<>.,?;:'()!~%\-_@#/*""\s]+$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(20, ErrorMessage = "More than 20 characters are not allowed.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [RegularExpression(@"^[^<>.,?;:'()!~%\-_@#/*""\s]+$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(20, ErrorMessage = "More than 20 characters are not allowed.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Marital Status is required")]
+        [Display(Name = "Marital Status")]
+        public string MaritalStatus { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Date Of Birth is required")]
+        [Display(Name = "Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Current Address is required")]
+        [Display(Name = "Current Address")]
+        public string CurrentAddress { get; set; }
+
+        [Required(ErrorMessage = "Pin Code is required.")]
+        [Display(Name = "Pin Code")]
+        [RegularExpression(@"^[1-9][0-9]{5}$", ErrorMessage = "Pin Code is invalid.")]
+        public Int32 PinCode { get; set; }
+
+        [Required(ErrorMessage = "Permanent Address is required")]
+        [Display(Name = "Permanent Address")]
+        public string PermanentAddress { get; set; }
+
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [Display(Name = "Mobile Number")]
+        [RegularExpression(@"[+][9][1][7-9][0-9]{9}$", ErrorMessage = "Mobile Number is invalid.")]
+        [StringLength(13, ErrorMessage = "Mobile Number should be of 10 characters long with +91 extension")]
+        public string MobileNo { get; set; }
+
+
+        [Required(ErrorMessage = "Team Role is required")]
+        [Display(Name = "Team Role")]
+        public string TeamRole { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Work Experience is required")]
+        [Display(Name = "Work Experience")]
+        [RegularExpression(@"^(([0-9]{1,2}(\.[0][0-9]))|([0-9]{1,2}(\.[1][0-1])))$", ErrorMessage = "Valid Decimal number with 2 decimal places.")]
+        public decimal WorkExperience { get; set; }
+
+        [Required(ErrorMessage = "Technology Known & Worked On is required")]
+        [Display(Name = "Technology Known")]
+        public string TechnologyKnown { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email Id is required")]
+        [Display(Name = "Email Id")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email Format is wrong.")]
+        [StringLength(50, ErrorMessage = "Less than 50 characters")]
+        public string EmailId { get; set; }
+
+
+        [Required(ErrorMessage = "Username is required")]
+        [RegularExpression(@"^[^<>,?;:'()!~%\-@#/*""\s]+$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(15, ErrorMessage = "More than 15 characters are not allowed.")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Passwod is required")]
+        [StringLength(15, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirmation of Passwod is required")]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and Confirmation Password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Security Question is required")]
+        [Display(Name = "Please select your Security Question?")]
+        public string SecurityQuestion { get; set; }
+
+
+        [Required(ErrorMessage = "Answer for Security Question is required")]
+        [Display(Name = "Enter your Answer")]
+        public string SecurityAnswer { get; set; }
+    }
+}
